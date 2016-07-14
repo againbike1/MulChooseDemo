b824549ae8e22c6467feaaf08c28603233a5f1f0
ywxiyue <esop@ailk-esop.local>
2016-07-14 14:14:58 +0800

    TableView、CollectView多选、单选

--- MulChooseDemo/TableVC_MulChoose.m
+++ MulChooseDemo/TableVC_MulChoose.m
@@ -25,12 +25,13 @@
     [super viewDidLoad];
 
     //增加数据
-    UIButton * btn = [[UIButton alloc]initWithFrame:CGRectMake(SCREEN_WIDTH-110, 0, 100, 50)];
-    [btn setTitle:@"AddArr" forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
-    [self.navigationController.navigationBar addSubview:btn];
-    
+//    UIButton * btn = [[UIButton alloc]initWithFrame:CGRectMake(SCREEN_WIDTH-110, 0, 100, 50)];
+//    [btn setTitle:@"AddArr" forState:UIControlStateNormal];
+//    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+//    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
+//    [self.navigationController.navigationBar addSubview:btn];
+//    UINavigationItem * right = [[UINavigationItem alloc]initWithTitle:@"AddArr"];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"AddArr" style:UIBarButtonItemStylePlain target:self action:@selector(click)];
     
     
     dataArr = [[NSMutableArray alloc]initWithCapacity:0];
--- MulChooseDemo/TableVC_SingleChoose.m
+++ MulChooseDemo/TableVC_SingleChoose.m
@@ -14,7 +14,6 @@
     NSMutableArray * dataArr;
 }
 
-
 @end
 
 @implementation TableVC_SingleChoose
@@ -26,12 +25,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //增加数据
-    UIButton * btn = [[UIButton alloc]initWithFrame:CGRectMake(SCREEN_WIDTH-110, 0, 100, 50)];
-    [btn setTitle:@"AddArr" forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
-    [self.navigationController.navigationBar addSubview:btn];
-    
+//    UIButton * btn = [[UIButton alloc]initWithFrame:CGRectMake(SCREEN_WIDTH-110, 0, 100, 50)];
+//    [btn setTitle:@"AddArr" forState:UIControlStateNormal];
+//    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+//    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
+//    [self.navigationController.navigationBar addSubview:btn];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"AddArr" style:UIBarButtonItemStylePlain target:self action:@selector(click)];
+
     
     
     dataArr = [[NSMutableArray alloc]initWithCapacity:0];
